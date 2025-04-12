780d622cfca0cdf4b9754c3526d4db5939fdc4fe
jadsonfe <jadson.a.f.s.l@gmail.com>
2025-04-12 16:33:05 -0300

    Feat: Add controller do workspace

--- src/controllers/WorkspaceController.js
+++ src/controllers/WorkspaceController.js
@@ -2,13 +2,33 @@ const { Workspace } = require('../models');
 // create workspace controller
 
 class WorkspaceController {
-    async index(req, res) {
-        const workspaces = await Workspace.findAll();
-        return res.json(workspaces);
-    }
     async store(req, res) {
         const { name } = req.body;
         const workspace = await Workspace.create({ name });
         return res.json(workspace);
     }
+    async index(req, res) {
+        const workspaces = await Workspace.findAll();
+        return res.json(workspaces);
+    }
+    async show(req, res) {
+        const { id } = req.params;
+        const workspace = await Workspace.findByPk(id);
+        if (!workspace) {
+            return res.status(404).json({ error: 'Workspace not found' });
+        }
+        return res.json(workspace);
+    }
+    async update(req, res) {
+        const { id } = req.params;
+        const { name } = req.body;
+        const workspace = await Workspace.findByPk(id);
+        if (!workspace) {
+            return res.status(404).json({ error: 'Workspace not found' });
+        }
+        await workspace.update({ name });
+        return res.json(workspace);
+    }
 }
+
+module.exports = new WorkspaceController();
